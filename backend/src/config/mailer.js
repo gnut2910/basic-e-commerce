42baf703bd05a0e2d8a888052cffe1b4ff93a1a8
gnut2910 <tunle4572@gmail.com>
2025-12-23 10:34:40 +0700

    fix mailer

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Kiểm tra kết nối SMTP khi server khởi chạy
